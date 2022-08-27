ff8eab733bc3486ebd2003865c300c53621124d1
benie254 <davinci.monalissa@gmail.com>
2022-08-27 11:40:10 +0300

    add diesel post request function

--- LogOnGo/src/app/components/forms/add-diesel-logform/add-diesel-logform.component.ts
+++ LogOnGo/src/app/components/forms/add-diesel-logform/add-diesel-logform.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FuelService } from 'src/app/services/fuel/fuel.service';
+import { LogService } from 'src/app/services/log/log.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Component({
   selector: 'app-add-diesel-logform',
@@ -7,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDieselLogformComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fuelService:FuelService, private notifService:NotificationService, private logService:LogService) { }
+
+  addLog(log_info: any) {
+    console.warn(log_info);
+    this.logService.addLog(log_info).subscribe((result) => {
+      console.warn('result', result);
+      this.notifService.submitSuccess('success','Diesel log added successfully!')
+      // this.notifService.showSuccess("Data posted successfully !!", "Notification")
+    });
+  }
 
   ngOnInit(): void {
   }
