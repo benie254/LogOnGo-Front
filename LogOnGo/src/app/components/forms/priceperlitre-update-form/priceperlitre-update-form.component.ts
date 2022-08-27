97a3d21117fb7301ca09b36dd70d9f1331548fbd
benie254 <davinci.monalissa@gmail.com>
2022-08-27 09:30:25 +0300

    add petrol details get & put functions

--- LogOnGo/src/app/components/forms/priceperlitre-update-form/priceperlitre-update-form.component.ts
+++ LogOnGo/src/app/components/forms/priceperlitre-update-form/priceperlitre-update-form.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FuelService } from 'src/app/services/fuel/fuel.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Component({
   selector: 'app-priceperlitre-update-form',
@@ -6,8 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./priceperlitre-update-form.component.css']
 })
 export class PriceperlitreUpdateFormComponent implements OnInit {
+  petrol_details: any; 
 
-  constructor() { }
+  constructor(private fuelService:FuelService, private notifService:NotificationService, private http:HttpClient) {
+    this.fuelService.getPetrolInfo().subscribe((data) => {
+      this.petrol_details = data
+      console.warn("data",data)
+    });
+   }
+
+  updatePetrolInfo(petrol_info:any){
+    console.warn(petrol_info);
+    this.fuelService.updatePetrolInfo(petrol_info).subscribe((result) => {
+      console.warn('updated result',result);
+      this.notifService.submitSuccess('success','Petrol info updated successfully!')
+    })
+  }
 
   ngOnInit(): void {
   }
