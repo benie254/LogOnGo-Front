4fc6159361e1a99088117cfd5f062514a4522f85
benie254 <davinci.monalissa@gmail.com>
2022-08-29 07:14:48 +0300

    add logic for all mpesa logs

--- LogOnGo/src/app/components/pages/all-logs/all-logs.component.ts
+++ LogOnGo/src/app/components/pages/all-logs/all-logs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogMpesaService } from 'src/app/services/log-mpesa/log-mpesa.service';
 import { LogService } from 'src/app/services/log/log.service';
 
 @Component({
@@ -7,11 +8,17 @@ import { LogService } from 'src/app/services/log/log.service';
   styleUrls: ['./all-logs.component.css']
 })
 export class AllLogsComponent implements OnInit {
-  logs: any;
+  all_logs: any;
+  all_mpesa_logs: any;
 
-  constructor(private logService:LogService) { 
+  constructor(private logService:LogService,private mpesaService:LogMpesaService) { 
     this.logService.getAllLogs().subscribe((data) => {
-      this.logs = data
+      this.all_logs = data
+      console.warn("data",data)
+    });
+
+    this.mpesaService.getAllMpesaLogs().subscribe((data) => {
+      this.all_mpesa_logs = data
       console.warn("data",data)
     });
   }
--- LogOnGo/src/app/services/log-mpesa/log-mpesa.service.ts
+++ LogOnGo/src/app/services/log-mpesa/log-mpesa.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LogMpesa } from 'src/app/classes/log-mpesa/log-mpesa';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,10 @@ export class LogMpesaService {
   apiURLaddMpesaLogs = 'https://logongo-api.herokuapp.com/add-mpesa-logs-today/'
 
   constructor(private http:HttpClient) { }
+
+  getAllLogs(): Observable<LogMpesa>{
+    return this.http.get<LogMpesa>(this.apiURLaddMpesaLogs);
+  }
 
   addMpesaLog(mpesa_info: any) {
     return this.http.post(this.apiURLaddMpesaLogs, mpesa_info);
