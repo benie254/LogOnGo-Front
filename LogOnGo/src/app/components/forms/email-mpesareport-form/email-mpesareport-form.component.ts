021eb1b11e46f3d17e6db8979891657f84041c29
benie254 <davinci.monalissa@gmail.com>
2022-08-29 06:28:26 +0300

    initiate logic for mpesa report

--- LogOnGo/src/app/components/forms/email-mpesareport-form/email-mpesareport-form.component.ts
+++ LogOnGo/src/app/components/forms/email-mpesareport-form/email-mpesareport-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailService } from 'src/app/services/email/email.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Component({
   selector: 'app-email-mpesareport-form',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailMpesareportFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private emailService:EmailService, private notifService:NotificationService) { }
+
+  emailMpesaReport(mpesa_report: any) {
+    console.warn(mpesa_report);
+    this.emailService.emailMpesaReport(mpesa_report).subscribe((result) => {
+      console.warn('result', result);
+      this.notifService.submitSuccess('success','Mpesa report sent to your email!')
+      // this.notifService.showSuccess("Data posted successfully !!", "Notification")
+    });
+  }
 
   ngOnInit(): void {
   }
