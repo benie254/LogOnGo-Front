6cfce810dee883ef79fd37735b5c8455a722d8d2
benie254 <davinci.monalissa@gmail.com>
2022-08-29 14:11:34 +0300

    update gas logs

--- LogOnGo/src/app/components/pages/gas-logs/gas-logs.component.ts
+++ LogOnGo/src/app/components/pages/gas-logs/gas-logs.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gas-logs.component.css']
 })
 export class GasLogsComponent implements OnInit {
+  date: any;
+  fuels: any;
+  info: any;
+  myAlert: any; 
+  logs: any;
+  logs_two: any;
+  logs_three: any;
+  logs_four: any;
+  mpesa: any;
+  id: number;
 
   constructor() { }
 
