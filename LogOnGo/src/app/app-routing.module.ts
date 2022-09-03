e095a634d2093b6f9630c278978c8c3359be2084
benie254 <davinci.monalissa@gmail.com>
2022-09-03 15:45:53 +0300

    update root component with login & logout logic

--- LogOnGo/src/app/app-routing.module.ts
+++ LogOnGo/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/auth/admin/admin.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'admin', component: AdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- LogOnGo/src/app/app.component.ts
+++ LogOnGo/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from './services/token/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'LogOnGo';
+  private roles: string[] = [];
+  isLoggedIn = false; 
+  showAdmin = false; 
+  username: string;
+
+  constructor(private tokenStorage:TokenStorageService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorage.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenStorage.getUser();
+      this.roles = user.roles; 
+      this.showAdmin = this.roles.includes('ROLE_ADMIN');
+      this.username = user.username
+    }
+  }
+  logout(): void {
+    this.tokenStorage.logout();
+    window.location.reload();
+  }
 }
