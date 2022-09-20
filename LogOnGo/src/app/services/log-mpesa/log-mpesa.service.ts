8ca883d184e3b1fad0222e0a2f7286679d59d1c2
benie254 <davinci.monalissa@gmail.com>
2022-09-20 18:06:20 +0300

    create a service for user mpesa logs

--- LogOnGo/src/app/services/log-mpesa/log-mpesa.service.ts
+++ LogOnGo/src/app/services/log-mpesa/log-mpesa.service.ts
@@ -10,11 +10,16 @@ export class LogMpesaService {
 
   // apiURLaddMpesaLogs = 'https://logongo.herokuapp.com/api/mpesa-logs-today/'
   apiURLaddMpesaLogs = 'http://127.0.0.1:8000/api/mpesa-logs-today/';
+  apiURLuserMpesaLogs = 'https://logongo.herokuapp.com/api/user-mpesa-logs/';
+  // apiURLuserMpesaLogs = 'http://127.0.0.1:8000/api/user-mpesa-logs/';
 
   constructor(private http:HttpClient) { }
 
   getAllMpesaLogs(): Observable<LogMpesa>{
     return this.http.get<LogMpesa>(this.apiURLaddMpesaLogs);
+  }
+  getUserMpesaLogs(id: any): Observable<LogMpesa>{
+    return this.http.get<LogMpesa>(this.apiURLuserMpesaLogs + id);
   }
 
   addMpesaLog(mpesa_info: LogMpesa) {
