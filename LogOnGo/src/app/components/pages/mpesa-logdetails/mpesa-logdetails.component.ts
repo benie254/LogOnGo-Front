cab877e7490789da00c62cc8ff652cf8e79a02da
benie254 <davinci.monalissa@gmail.com>
2022-09-22 18:14:50 +0300

    create mpesa add log logic

--- LogOnGo/src/app/components/pages/mpesa-logdetails/mpesa-logdetails.component.ts
+++ LogOnGo/src/app/components/pages/mpesa-logdetails/mpesa-logdetails.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import * as Notiflix from 'notiflix';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { LogMpesaService } from 'src/app/services/log-mpesa/log-mpesa.service';
 
 @Component({
@@ -9,16 +12,36 @@ import { LogMpesaService } from 'src/app/services/log-mpesa/log-mpesa.service';
 })
 export class MpesaLogdetailsComponent implements OnInit {
   mpesaDetails: any; 
+  id: number;
+  mpesaDetailsForm: FormGroup;
+  formBuilder: FormBuilder;
+  currentUser = this.authService.currentUserValue;
 
-  
+
   constructor(
-    private mpesaService:LogMpesaService
+    private mpesaService:LogMpesaService,
+    private route:ActivatedRoute,
+    private authService:AuthService,
   ) { }
 
   ngOnInit(): void {
+    // this.id = this.route.snapshot.params['id'];
+    this.route.params.subscribe(params => this.getMpesaLogDetails(params['id']))
+    this.mpesaDetailsForm = new FormGroup({
+      date: new FormControl('',Validators['date']),
+      transaction_number: new FormControl('',Validators['required']),
+      customer_name: new FormControl('',Validators['required']),
+      customer_phone_number: new FormControl(0,Validators['required']),
+      amount: new FormControl(0,Validators['required']),
+      amount_transferred_to_bank: new FormControl(0),
+      user: new FormControl(0),
+      logged_by: new FormControl('')
+    });
   }
 
-  getLogDetails(id:number){
+  get f() { return this.mpesaDetailsForm.controls; }
+
+  getMpesaLogDetails(id:number){
     this.mpesaService.getMpesaLogDetails(id).subscribe(
       (data) => {
         this.mpesaDetails = data
@@ -30,6 +53,19 @@ export class MpesaLogdetailsComponent implements OnInit {
         Notiflix.Notify.failure('Something went wrong!')
       }
     );
+  }
+
+  addMpesaLog(mpesa_info: any) {
+    this.mpesaService.addMpesaLog(mpesa_info).subscribe((result) => {
+      console.warn('result', result);
+      Notiflix.Notify.success('Mpesa log added successful!');
+      this.ngOnInit();
+    }, 
+    err => {
+      console.warn(err)
+      Notiflix.Notify.failure('Something went wrong!');
+      Notiflix.Notify.warning('Please try again.');
+    });
   }
 
 }
