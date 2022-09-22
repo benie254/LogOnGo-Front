f6cb1c05930687bb861315fd5ae7ed4c8e4be7fb
benie254 <davinci.monalissa@gmail.com>
2022-09-22 19:03:27 +0300

    create a service for email mpesa report

--- LogOnGo/src/app/services/report/report.service.ts
+++ LogOnGo/src/app/services/report/report.service.ts
@@ -7,6 +7,8 @@ import { Injectable } from '@angular/core';
 export class ReportService {
   // apiURLlogReport = 'https://logongo.herokuapp.com/api/email-report/';
   apiURLlogReport = 'http://127.0.0.1:8000/api/email-report/';
+  // apiMpesaReport = 'https://logongo.herokuapp.com/api/email-mpesa-report/';
+  apiMpesaReport = 'http://127.0.0.1:8000/api/email-mpesa-report/';
 
   constructor(
     private http:HttpClient,
@@ -14,5 +16,8 @@ export class ReportService {
 
   emailLogReport(report_info: any) {
     return this.http.post(this.apiURLlogReport, report_info);
+  }
+  emailMpesaReport(report_info: any) {
+    return this.http.post(this.apiMpesaReport, report_info);
   }
 }
