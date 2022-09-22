a9d4b932622069ce1b75011d3075e4a9a50958f6
benie254 <davinci.monalissa@gmail.com>
2022-09-22 18:49:16 +0300

    restructure component & update details logic

--- LogOnGo/src/app/components/pages/print-mpesa/print-mpesa.component.ts
+++ LogOnGo/src/app/components/pages/print-mpesa/print-mpesa.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as Notiflix from 'notiflix';
+import { LogMpesaService } from 'src/app/services/log-mpesa/log-mpesa.service';
 
 @Component({
   selector: 'app-print-mpesa',
@@ -6,13 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./print-mpesa.component.css']
 })
 export class PrintMpesaComponent implements OnInit {
-  mpesa_log_details: any; 
+  mpesaDetails: any; 
   mpesa_total: any; 
   mpesa_cumulative: any;
 
-  constructor() { }
+  constructor(
+    private mpesaService:LogMpesaService,
+    private route:ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => this.getMpesaLogDetails(params['id']))
+  }
+
+  getMpesaLogDetails(id:number){
+    this.mpesaService.getMpesaLogDetails(id).subscribe(
+      (data) => {
+        this.mpesaDetails = data
+        console.warn("mpesa log details",data)
+        Notiflix.Notify.success('Get mpesa details success')
+      },
+      err => {
+        console.warn(err)
+        Notiflix.Notify.failure('Something went wrong!')
+      }
+    );
   }
 
 }
