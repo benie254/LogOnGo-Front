b82ae162c2e81f74be10bbc48bf2fb9211370ec1
benie254 <davinci.monalissa@gmail.com>
2022-10-04 16:58:41 +0300

    restructure template & update styles

--- LogOnGo/src/app/components/sections/all-mpesa-card/all-mpesa-card.component.ts
+++ LogOnGo/src/app/components/sections/all-mpesa-card/all-mpesa-card.component.ts
@@ -9,18 +9,32 @@ import { LogService } from 'src/app/services/log/log.service';
 })
 export class AllMpesaCardComponent implements OnInit {
   mpesa_logs: any;
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 5;
+  tableSizes: any = [2, 5, 10, 15];
 
   constructor(
     private mpesaService:LogMpesaService,
-  ) {
-    this.mpesaService.getAllMpesaLogs().subscribe(
-      (data) => {
-      this.mpesa_logs = data
-      console.warn("data",data)
-    });
-   }
+  ) { }
 
   ngOnInit(): void {
+    this.getAllMpesaLogs();
+  }
+  getAllMpesaLogs(){
+    this.mpesaService.getAllMpesaLogs().subscribe((data) => {
+      this.mpesa_logs = data
+      console.warn("all mpesa_logs data",data)
+    });
+  }
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.getAllMpesaLogs();
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.getAllMpesaLogs();
   }
 
 }
