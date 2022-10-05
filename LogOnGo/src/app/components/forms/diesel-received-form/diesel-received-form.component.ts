ddff1f660d288a7e3c8a26fac3492bbbd76b899f
benie254 <davinci.monalissa@gmail.com>
2022-10-05 07:30:45 +0300

    restructure gas received form & update logic

--- LogOnGo/src/app/components/forms/diesel-received-form/diesel-received-form.component.ts
+++ LogOnGo/src/app/components/forms/diesel-received-form/diesel-received-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Notiflix from 'notiflix';
 import { FuelService } from 'src/app/services/fuel/fuel.service';
 import { LogService } from 'src/app/services/log/log.service';
 import { NotificationService } from 'src/app/services/notification/notification.service';
@@ -15,20 +16,29 @@ export class DieselReceivedFormComponent implements OnInit {
     private fuelService:FuelService, 
     private notifService:NotificationService, 
     private logService:LogService
-    ) { }
+    ) {
+      this.fuelService.getDieselInfo().subscribe(
+        (data) => {
+          this.dieselInfo = data;
+        }
+      )
+     }
 
   dieselReceived(diesel_received: any) {
     console.warn(diesel_received);
-    this.fuelService.addDieselReceived(diesel_received).subscribe((result) => {
+    Notiflix.Loading.dots('Processing...')
+    this.fuelService.addFuelReceived(diesel_received).subscribe((result) => {
       console.warn('result', result);
       // this.notifService.submitSuccess('success','Diesel received added successfully!')
       // this.notifService.showSuccess("Data posted successfully !!", "Notification")
-    });
-    this.fuelService.getDieselInfo().subscribe(
-      (data) => {
-        this.dieselInfo = data;
-      }
-    )
+      Notiflix.Loading.remove();
+      Notiflix.Notify.success('Add success!');
+    },
+    err => {
+      Notiflix.Notify.failure('Add failed!');
+      Notiflix.Loading.remove();
+    }
+    );
   }
 
   ngOnInit(): void {
--- LogOnGo/src/app/components/forms/gas-received-form/gas-received-form.component.ts
+++ LogOnGo/src/app/components/forms/gas-received-form/gas-received-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Notiflix from 'notiflix';
 import { FuelService } from 'src/app/services/fuel/fuel.service';
 import { LogService } from 'src/app/services/log/log.service';
 import { NotificationService } from 'src/app/services/notification/notification.service';
@@ -9,16 +10,35 @@ import { NotificationService } from 'src/app/services/notification/notification.
   styleUrls: ['./gas-received-form.component.css']
 })
 export class GasReceivedFormComponent implements OnInit {
+  gasInfo: any;
 
-  constructor(private fuelService:FuelService, private notifService:NotificationService, private logService:LogService) { }
+  constructor(
+    private fuelService:FuelService, 
+    private notifService:NotificationService, 
+    private logService:LogService
+    ) { 
+      this.fuelService.getDieselInfo().subscribe(
+        (data) => {
+          this.gasInfo = data;
+        }
+      )
+    }
 
   gasReceived(gas_received: any) {
     console.warn(gas_received);
-    this.fuelService.addGasReceived(gas_received).subscribe((result) => {
+    Notiflix.Loading.dots('Processing...')
+    this.fuelService.addFuelReceived(gas_received).subscribe((result) => {
       console.warn('result', result);
-      // this.notifService.submitSuccess('success','Gas received added successfully!')
+      // this.notifService.submitSuccess('success','Diesel received added successfully!')
       // this.notifService.showSuccess("Data posted successfully !!", "Notification")
-    });
+      Notiflix.Loading.remove();
+      Notiflix.Notify.success('Add success!');
+    },
+    err => {
+      Notiflix.Notify.failure('Add failed!');
+      Notiflix.Loading.remove();
+    }
+    );
   }
 
   ngOnInit(): void {
